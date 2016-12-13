b1a04233a441eede110a6d0a25f82130348f97d8
xujian <xujian@dengyuecang.com>
2016-12-13 17:15:10 +0800

    兴趣分类 实体类

--- src/com/dengyuecang/www/entity/circle/InterestType.java
+++ src/com/dengyuecang/www/entity/circle/InterestType.java
@@ -1,7 +1,49 @@
 package com.dengyuecang.www.entity.circle;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * Created by acang on 2016/12/12.
  */
-public class InterestType {
+@Entity
+@Table(name = "dyc_circle_interest_type")
+public class InterestType implements Serializable{
+
+    private String id;
+
+    private String name;
+
+    private String parent_id;
+
+    @GenericGenerator(name = "generator", strategy = "uuid")
+    @Id
+    @GeneratedValue(generator = "generator")
+
+    @Column(name = "id", unique = true, nullable = false, length = 100)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParent_id() {
+        return parent_id;
+    }
+
+    public void setParent_id(String parent_id) {
+        this.parent_id = parent_id;
+    }
 }
