0226aa4df0d8ff9e155072c214db533620817601
xujian <xujian@dengyuecang.com>
2016-12-21 14:44:43 +0800

    兴趣档案

--- src/com/dengyuecang/www/controller/api/circle/InterestTypeController.java
+++ src/com/dengyuecang/www/controller/api/circle/InterestTypeController.java
@@ -2,8 +2,10 @@ package com.dengyuecang.www.controller.api.circle;
 
 import com.dengyuecang.www.service.circle.IInformationService;
 import com.dengyuecang.www.service.circle.IInterestTypeService;
+import com.dengyuecang.www.utils.RespCode;
 import com.dengyuecang.www.utils.RespData;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +24,16 @@ public class InterestTypeController {
 
     @RequestMapping("/list")
     @ResponseBody
-    public RespData interestType(HttpHeaders headers){
+    public RespData interestType(@RequestHeader HttpHeaders headers){
 
-        return interestTypeServiceImpl.queryList();
+        try {
+            return interestTypeServiceImpl.queryList();
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+        return RespCode.getRespData(RespCode.UNKNOW_EXCEPTION);
 
     }
 
